2f707e4d4ceb4f0c40b9c17bf3be07c1a675ff4a
jenilprince <jenilprince2007@gmail.com>
2025-07-08 17:38:59 +0530

    Delete pie chart.py

--- pie chart.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-data=pd.read_csv('data.csv')
-print(data)
-plt.pie(data["Maths"],labels=data["Name"],autopct="%1.2f%%",startangle=0,colors=["lightcoral","lightblue","lightyellow","lightgreen"],explode=[0.05,0.05,0.05,.05],wedgeprops={"width":.7})
-plt.title("Maths",fontsize=20,fontweight="bold")
-plt.axis('equal')
-plt.show()
