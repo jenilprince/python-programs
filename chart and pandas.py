2f8d7b6c0351d36d47515c17fbb149f81222501d
jenilprince <jenilprince2007@gmail.com>
2025-07-08 17:37:51 +0530

    Delete chart and pandas.py

--- chart and pandas.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-data=[88,99,42,55]
-labels=["alex","anu","jack","man"]
-colors=["red","green","blue","yellow"]
-explode=[0.0,0.0,0.1,0.0]
-plt.pie(data,labels=labels,colors=colors, autopct="%1.0f%%",explode=explode)
-plt.title("marks")
-plt.show()
