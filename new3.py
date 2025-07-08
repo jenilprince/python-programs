a299bd0238405c133a208eb65d1a3cfd5c7d64d0
jenilprince <jenilprince2007@gmail.com>
2025-07-08 17:40:37 +0530

    Delete new3.py

--- new3.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-data=pd.read_csv('data.csv')
-data["Total"]=data["Maths"]+data["Science"]+data["English"]
-hig=data[data["Total"]>265]
-print(data[["Science","Maths","Total"]])
-
-
-
-
